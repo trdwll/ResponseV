a780353af0dba0db3222ae51117a35684ec2962f
trdwll <git@trdwll.com>
2017-10-06 16:39:43 -0400

    create project

---
+++ src/ResponseV/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LSPD_First_Response.Mod.API;
+namespace ResponseV
+{
+    public class Main : Plugin
+    {
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Finally()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
